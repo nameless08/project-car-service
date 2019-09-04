cdd228f1fd1eea8631ce82a214ad4d723775243a
Aleksandr Novikov <new0808@yandex.ru>
2019-09-04 15:10:14 +0300

    Файлы верстки учебного проекта

---
+++ html/assets/page-contacts/pub/js/main.js
@@ -0,0 +1,19 @@
+
+
+;(function($, undefined){ 
+
+   	var Closed = false;
+
+	$('.hamburger').click(function () {
+        if (Closed == true) {
+          $(this).removeClass('open');
+          $(this).addClass('closed');
+          Closed = false;
+        } else {               
+          $(this).removeClass('closed');
+          $(this).addClass('open');
+          Closed = true;
+        }
+	});
+
+})(jQuery);
